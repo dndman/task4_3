5342bc3cdce56d19e4230974c3352de738cb997a
dndman <alexey.kalenichenko@gmail.com>
2018-04-02 00:50:36 +0300

    backup chaos

--- task4_3.sh
+++ task4_3.sh
@@ -21,4 +21,34 @@ fi
 if [ ! -d /tmp/backups ]; then
  mkdir -p /tmp/backups
 fi
-PREF="tar.gz"
+PREF=`echo ${BAKDIR:1}|sed 's/\//-/g'`
+
+if [ -f "/tmp/backups/${PREF}.tar.gz" ]; then
+  mv "/tmp/backups/${PREF}.tar.gz" "/tmp/backups/${PREF}.0.tar.gz"
+fi
+
+
+
+#COUNT=`ls -1 /tmp/backups |grep "$PREF"| wc -l`
+SURVIVER=`ls -1 /tmp/backups |grep "$PREF"| sort | head -n$((BAKNUM-1))`
+#DEADS=`ls -1 /tmp/backups |grep "$PREF"| sort | tail -n$((COUNT - BAKNUM + 1))`
+
+
+for FILE in `ls -1 /tmp/backups |grep "$PREF"`; do
+  if ! [[ $SURVIVER = *"$FILE"* ]]; then
+  rm -f "/tmp/backups/$FILE"
+  fi
+done
+
+
+if (( "$BAKNUM" > 1 )); then 
+
+ for N in $(seq $((BAKNUM-1)) -1 1); do
+   if [ -f "/tmp/backups/${PREF}.$((N-1)).tar.gz" ]; then
+   mv "/tmp/backups/${PREF}.$((N-1)).tar.gz" "/tmp/backups/${PREF}.${N}.tar.gz"
+   fi
+ done
+fi
+
+tar -cf "/tmp/backups/${PREF}.tar.gz" "${BAKDIR}" 2>/dev/null
+
